147aa277f85a03729df2e20f905dd8a0b53028e6
Aleksandar Stojanovic <aleksandar.stojanovic19982@gmail.com>
2025-05-03 14:01:45 +0200

    Zamenjen bcrypt sa bcryptjs

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 })
 
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 mongoose.connect("mongodb://127.0.0.1:27017/Dzoni", {
     useNewUrlParser: true,
