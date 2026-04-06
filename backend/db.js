f4666df4d91113940fab18a2163e4bf7848657cc
Aleksandar Stojanovic <aleksandar.stojanovic19982@gmail.com>
2026-04-06 15:17:35 +0200

    Dodati docker file i docker compose config/back

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,29 @@
+// const mongoose = require('mongoose');
+
+// // Povezivanje sa mongoDB bazom podataka (ovde koristimo MongoDB Atlas URL)
+// mongoose.connect('mongodb://localhost:27017/Dzoni', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then(() => {
+//     console.log("Uspesno poveza sa bazom");
+// })
+// .catch(err => {
+//     console.error("Greska pri povezivanju sa bazom:", err);
+// })
+
+
 const mongoose = require('mongoose');
 
-// Povezivanje sa mongoDB bazom podataka (ovde koristimo MongoDB Atlas URL)
-mongoose.connect('mongodb://localhost:27017/Dzoni', {
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/Dzoni';
+
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => {
-    console.log("Uspesno poveza sa bazom");
+    console.log("Uspesno povezan sa bazom");
 })
 .catch(err => {
     console.error("Greska pri povezivanju sa bazom:", err);
-})
+});
