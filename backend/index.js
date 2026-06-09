51b5147025bbd290322293a099b2fe0f12a822c7
Aleksandar Stojanovic <aleksandar.stojanovic19982@gmail.com>
2026-06-09 15:03:09 +0200

    Run CD after CI for all changes

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.get("/health", (req, res) => {
 
 app.get("/instance", (req, res) => {
     res.status(200).json({
-        pod: process.env.HOSTNAME,
+        pod: process.env.HOSTNAME || "local-machine",
         message: "Request handeled by this backnd pod"
     });
 });
